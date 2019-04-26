6b0c1500d22e413938992638f291177633def74b
BlazingRockStorm <rocker.gryphon@gmail.com>
2019-04-26 19:22:34 +0700

    add search

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,11 @@
 class Place < ApplicationRecord
+    include SearchCop
+    
     has_one_attached :picture
     has_many :events
+
+    search_scope :search do
+        attributes :name, :description
+        # ...
+    end
 end
