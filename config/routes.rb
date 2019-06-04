f46ebd0db1740ab7b01f07eee8b6bec59dc84f01
dinhthai2206 <dinhthai2206@gmail.com>
2019-06-04 14:41:28 +0700

    remove hidden status

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,5 @@ Rails.application.routes.draw do
     end
   end
   resources :searches
-  put 'events/:id/public' => 'events#public_event', as: 'public_event'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20190311081417_add_hidden_status_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddHiddenStatusToEvents < ActiveRecord::Migration[5.2]
-  def change
-    add_column :events, :hidden_status, :boolean
-  end
-end
